5afcb307e1e4ed0273f66c6aa15e49e11ad72b45
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-06 13:01:11 +0600

    dumpdata and content model

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -38,3 +40,12 @@ class Module(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
