f9e6c31165468a1fdd91ea3242c746506e39dcec
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-07 04:00:43 +0600

    course content create

--- courses/urls.py
+++ courses/urls.py
@@ -3,9 +3,20 @@ from . import views
 
 
 urlpatterns = [
-    path('me/', views.ManageCourseListView.as_view(), name='manage_course_list'),
-    path('create/', views.CourseCreateView.as_view(), name='course_create'),
-    path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
-    path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('me/', views.ManageCourseListView.as_view(),
+         name='manage_course_list'),
+    path('create/', views.CourseCreateView.as_view(),
+         name='course_create'),
+    path('<pk>/edit/', views.CourseUpdateView.as_view(),
+         name='course_edit'),
+    path('<pk>/delete/', views.CourseDeleteView.as_view(),
+         name='course_delete'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
+         name='course_module_update'),
+    path('module/<int:module_id>/content/<model_name>/create',
+         views.ContentCreateUpdateView.as_view(),
+         name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>/',
+         views.ContentCreateUpdateView.as_view(),
+         name='module_content_update'),
 ]
