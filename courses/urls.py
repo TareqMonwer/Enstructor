9f218e37cabce96d91367869869648d4ddf6f28c
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-07 18:09:31 +0600

    course module & content management

--- courses/urls.py
+++ courses/urls.py
@@ -19,4 +19,10 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/<id>/',
          views.ContentCreateUpdateView.as_view(),
          name='module_content_update'),
+    path('content/<int:id>/delete/',
+         views.ContentDeleteView.as_view(),
+         name='module_content_delete'),
+    path('module/<int:module_id>/',
+         views.ModuleContentListView.as_view(),
+         name='module_content_list'),
 ]
