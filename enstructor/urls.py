2268315dbef7ddd478a039c711249acc6f9eae63
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-15 03:50:34 +0600

    basic ui update: bootswatch theme added.

--- enstructor/urls.py
+++ enstructor/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from courses.views import CourseListView
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name = 'login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
